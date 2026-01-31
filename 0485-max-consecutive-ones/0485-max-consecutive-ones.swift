ef003f180caf150511baefddb3c9c23a54dfdb83
Angshuman <100036367+Angshuman-das@users.noreply.github.com>
2026-01-31 22:17:48 +0530

    Time: 0 ms (100%), Space: 20.1 MB (94.4%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        var count = 0, maxCount = 0
+
+        for num in nums {
+            if num == 1 {
+                count += 1
+            } else {
+                count = 0 
+            }
+
+            maxCount = max(maxCount, count)
+        }
+
+        return maxCount
+    }
+}
