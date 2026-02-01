15691cbdeb169628ebe426218aa97d88f2b03d20
Angshuman <100036367+Angshuman-das@users.noreply.github.com>
2026-02-01 10:23:52 +0530

    Time: 0 ms (100%), Space: 19.6 MB (76.62%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var l = 0, h = nums.count - 1
+
+        while l <= h {
+            var m = (l + h) / 2
+
+            if nums[m] == target {
+                return m
+            } else if nums[m] > target {
+                h = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+
+        return l
+    }
+}
