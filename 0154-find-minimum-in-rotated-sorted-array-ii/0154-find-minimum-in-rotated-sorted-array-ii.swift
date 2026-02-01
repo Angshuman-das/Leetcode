895ed1c392772df8d20b7be80a5c32a4144e8e97
Angshuman <100036367+Angshuman-das@users.noreply.github.com>
2026-02-01 14:17:29 +0530

    Time: 0 ms (100%), Space: 20.4 MB (7.69%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func findMin(_ nums: [Int]) -> Int {
+        var mini = Int.max
+        var l = 0, h = nums.count - 1
+
+        while l <= h {
+            var m = (l + h) / 2
+
+            if nums[l] == nums[m] && nums[m] == nums[h] {
+                mini = min(mini, nums[l])
+                l += 1
+                h -= 1
+            } else if nums[l] <= nums[m] {
+                mini = min(mini, nums[l])
+                l = m + 1
+            } else {
+                mini = min(mini, nums[m])
+                h = m - 1
+            }
+        }
+
+        return mini
+    }
+}
